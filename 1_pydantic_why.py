295cda3cc64b4aa67d43ad3740c2a2eab8aaa169
Nikhil Kumar <92905941+Nick1200000@users.noreply.github.com>
2025-05-22 06:29:45 +0000

    Learning the Type and Data Validation using Pydantic

---
+++ 1_pydantic_why.py
@@ -0,0 +1,32 @@
+#Pydantic helps us to resolve 2 problems
+#1. Type Validation
+#2. Data Validation
+
+def insert_patient_data(name: str, age: int): # name:str - > Type hinting
+
+    if type(name) == str and type(age) == int: #This Type Validation is not a production grade code 
+        if age < 0 :
+            raise ValueError("Age can't be negative")
+        else:
+            print(name)
+            print(age)
+            print("Inserted in the DB")
+    else:
+        raise TypeError("Incorrect Type")
+
+def update_patient_data(name: str, age: int):
+    
+    if type(name) == str and type(age) == int:
+
+        if age < 0:
+            raise ValueError('Age Can't be negative) # This is called an Data Validation 
+        else:
+            print(name)
+            print(age)
+            print("Updated in the DB")
+    else:
+        raise TypeError("Incorrect data type")
+
+
+
+insert_patient_data('nikhil', '30')
---
+++ 2_pydantic_type_validation.py
@@ -0,0 +1,36 @@
+from pydantic import BaseModel
+from typing import List, Dict, Optional
+class Patient(BaseModel):
+
+    name: str
+    age: int
+    weight: float
+    married: bool = False # setting an default value
+    allergies: Optional[List[str]] = None
+    contact_details: Dict[str, str]
+
+
+def insert_patient_data(patient: Patient):
+    print(patient.name)
+    print(patient.age)
+    print(patient.weight)
+    print(patient.married)
+    print(patient.allergies)
+    print(patient.contact_details)
+    print("Inserted in the DB")
+
+def update_patient_data(patient: Patient):
+    print(patient.name)
+    print(patient.age)
+    print(patient.weight)
+    print(patient.married)
+    print(patient.allergies)
+    print(patient.contact_details)
+    print("Updated Successfully")
+
+patient_info = {'name':'nikhil', 'age': 23, 'weight': 75.2, 'married': True, 'allergies': ['pollen', 'dust'], 'contact_details':{'email':'abcd@gmail.com', 'phone': '2333434'}}
+
+patient1 = Patient(**patient_info)
+
+insert_patient_data(patient1)
+update_patient_data(patient1)
